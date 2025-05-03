973d732f0be63d76b78f67bef11ce3912487c90c
Pascal Audet <pascal.audet@uottawa.ca>
2025-05-03 20:31:36 +1200

    deploy docs on merge

--- docs/conf.py
+++ docs/conf.py
@@ -22,8 +22,7 @@ copyright = '2019, Pascal Audet'
 author = 'Pascal Audet'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
-
+from platecurie import __version__ as release
 
 # -- General configuration ---------------------------------------------------
 
